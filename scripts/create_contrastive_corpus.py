42fab465dbd281301a4f863c9397c0a98264a535
Aryo Pradipta Gema <aryopg@gmail.com>
2023-12-01 01:16:16 +0000

    create contrastive corpus automatically to test faithfulness

--- scripts/create_contrastive_corpus.py
+++ scripts/create_contrastive_corpus.py
@@ -1,67 +1,86 @@
 import argparse
 import json
-import random
+from typing import List
 
 import pandas as pd
 import spacy
-from metamap import MetaMap
+from tqdm import tqdm
+
+ALLOCATED_KEYWORDS = {
+    "primary",
+    "secondary",
+    "primary trial",
+    "secondary trial",
+    "eligibility",
+    "adverse event",
+    "adverse events",
+    "intervention",
+    "result",
+    "results",
+    "participant",
+    "participants",
+}
 
 
 def argument_parser():
-    parser = argparse.ArgumentParser(description="Clinical PEFT")
+    parser = argparse.ArgumentParser(
+        description="Contrastive dataset for internal faithfulness test"
+    )
     parser.add_argument(
         "--data_path",
         type=str,
         required=True,
         help="Path to the input clinical text file",
     )
-    parser.add_argument(
-        "--metamap_path",
-        type=str,
-        required=True,
-        help="Path to the MetaMap installation",
-    )
     args = parser.parse_args()
     return args
 
 
-# Initialize MetaMap
-def initialize_metamap(metamap_path):
-    return MetaMap.get_instance(metamap_path)
+def get_synonyms_for_entity(entity, linker):
+    def is_valid_alias(text):
+        if "," in text:
+            return False
+        if "(" in text or ")" in text:
+            return False
+        if "(" in text:
+            return False
+        return True
 
-
-# Function to get synonyms for an entity using MetaMap
-def get_synonyms_for_entity(entity_text, mm_instance):
     synonyms = set()
 
-    # Extract concepts using MetaMap
-    concepts, error = mm_instance.extract_concepts([entity_text])
-
-    if not error and concepts:
-        for concept in concepts[0]:
-            preferred_name = concept.preferred_name
-            synonyms.update(concept.synonyms)
+    # Link entities to their CUI
+    for umls_ent in entity._.kb_ents:
+        # umls_ent is a Tuple(CUI, confidence_score)
+        linked_entity = linker.kb.cui_to_entity[umls_ent[0]]
+        # Update to the synonyms set
+        for alias in linked_entity.aliases:
+            if is_valid_alias(alias):
+                synonyms.update([alias.lower()])
 
     # Remove the original entity text from the set
-    synonyms.discard(entity_text)
+    synonyms.discard(entity.text.lower())
 
     return list(synonyms)
 
 
 # Function to replace entities with synonyms
-def replace_entities_with_synonyms(text, nlp, mm_instance):
+def replace_entities_with_synonyms(text, nlp):
     doc = nlp(text)
 
     replaced_texts = []
     for ent in doc.ents:
+        # Skipping generic entity names, just in case
+        if ent.text.lower() in ALLOCATED_KEYWORDS:
+            continue
+
         # Use a method to get synonyms for the entity text
-        synonyms = get_synonyms_for_entity(ent.text, mm_instance)
+        synonyms = get_synonyms_for_entity(ent, nlp.get_pipe("scispacy_linker"))
 
         # Replace the entity with a random synonym
         if synonyms:
-            replacement = random.choice(synonyms)
-            replaced_text = text.replace(ent.text, replacement)
-            replaced_texts += [replaced_text]
+            for synonym in synonyms:
+                replaced_text = text.replace(ent.text, synonym)
+                replaced_texts += [replaced_text]
 
     return replaced_texts
 
@@ -70,15 +89,25 @@ def main():
     # Parse command line arguments
     args = argument_parser()
 
-    # Load the ScispaCy model
-    nlp = spacy.load("en_core_sci_sm")
+    # Load the ScispaCy model:
+    # BC5CDR model only extracts chemical and diseases
+    nlp = spacy.load("en_ner_bc5cdr_md")
 
-    # Initialize MetaMap
-    mm_instance = initialize_metamap(args.metamap_path)
+    # Add the abbreviation pipe to the spacy pipeline.
+    nlp.add_pipe("abbreviation_detector")
+
+    # Add the entity linking pipe to the spacy pipeline
+    linker_configs = {
+        "resolve_abbreviations": True,
+        "threshold": 0.85,
+        "max_entities_per_mention": 1,
+        "linker_name": "umls",
+    }
+    nlp.add_pipe("scispacy_linker", config=linker_configs)
 
     # Read text from the specified file
     # Load train data
-    df = pd.read_json(args.train_data_path)
+    df = pd.read_json(args.data_path)
     df = df.transpose()
     # Extract statements
     statement_ids = df.index.tolist()
@@ -86,14 +115,18 @@ def main():
 
     # Replace entities with synonyms
     modified_statements_dict = dict()
-    for statement_id, statement in zip(statement_ids, statements):
-        modified_statements = replace_entities_with_synonyms(
-            statement, nlp, mm_instance
-        )
+    i = 0
+    for statement_id, statement in tqdm(
+        zip(statement_ids, statements), total=len(statement_ids)
+    ):
+        modified_statements: List[str] = replace_entities_with_synonyms(statement, nlp)
         modified_statements_dict[statement_id] = {
             "original": statement,
             "modified": modified_statements,
         }
+        if i >= 50:
+            break
+        i += 1
 
     # Save the dictionary to a JSON file
     data_name = args.data_path.split("/")[-1].replace(".json", "")
