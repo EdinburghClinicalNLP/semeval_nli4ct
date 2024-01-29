be4f02dbc3f3f69d4ffc9787077cd1a53aa3d28d
aryopg@gmail.com <aryopg@gmail.com>
2024-01-29 20:40:28 +0000

    set adapters in use only if adapters are initialised

--- src/models/chat_model_pipeline.py
+++ src/models/chat_model_pipeline.py
@@ -206,7 +206,8 @@ class ChatModelPipeline:
             section_name = inputs["section"][0].lower().replace(" ", "_")
             adapters_in_use += [section_name]
 
-        self.model.set_adapter(adapters_in_use)
+        if adapters_in_use:
+            self.model.set_adapter(adapters_in_use)
 
         outputs = self.model(
             input_ids=model_input, attention_mask=attention_mask, labels=labels
@@ -249,7 +250,8 @@ class ChatModelPipeline:
                     section_name = inputs["section"][0].lower().replace(" ", "_")
                     adapters_in_use += [section_name]
 
-                self.model.set_adapter(adapters_in_use)
+                if adapters_in_use:
+                    self.model.set_adapter(adapters_in_use)
 
                 output = self.model.generate(
                     input_ids=model_input,
