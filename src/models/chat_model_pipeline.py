525b27a064bfceea2a79d8143de1b88f13637be5
aryopg@gmail.com <aryopg@gmail.com>
2024-01-30 14:23:45 +0000

    fix bug

--- src/models/chat_model_pipeline.py
+++ src/models/chat_model_pipeline.py
@@ -47,7 +47,7 @@ class ChatModelPipeline:
     def load_pretrained_adapters(self) -> dict:
         adapter_names = []
         for i, pretrained_adapter_path in enumerate(
-            self.model_configs.pretrained_adapter_paths
+            self.model_configs.configs.pretrained_adapter_paths
         ):
             adapter_name = pretrained_adapter_path.split("_")[-1]
             if i == 0:
