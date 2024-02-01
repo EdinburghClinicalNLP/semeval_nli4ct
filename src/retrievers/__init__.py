1cb4bd06adacd11e56429756c90530ffa28411fe
aryopg@gmail.com <aryopg@gmail.com>
2024-02-01 21:24:11 +0000

    add random retrievers as baseline

--- src/retrievers/__init__.py
+++ src/retrievers/__init__.py
@@ -1,3 +1,4 @@
 from .bm25_okapi import CTRBM25Okapi
 from .bm25_okapi_dense_reranker import CTRBM25OkapiDenseReranker
 from .bm25_okapi_length_penalty import CTRBM25OkapiLengthPenalty
+from .random import Random
